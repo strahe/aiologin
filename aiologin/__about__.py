5f23e721ae22c5aa024a3340e00bd7921d2bb485
Konstantin Itskov <konstantin.itskov@kovits.com>
2016-06-21 17:33:19 -0400

    about module containing all of the information related to the module.

---
+++ aiologin/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'aiologin'
+__description__ = 'User session management for aiohttp'
+__url__ = 'https://github.com/trivigy/aiologin'
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Konstantin Itskov'
+__author_email__ = 'konstantin.itskov@kovits.com'
+__license__ = 'MIT'
+__copyright__ = '(c) 2016 by Konstantin Itskov'
